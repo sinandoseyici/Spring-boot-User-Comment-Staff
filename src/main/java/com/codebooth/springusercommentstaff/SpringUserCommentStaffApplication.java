028696d88c780593d45965c1b1e616025bbcdb51
Sinan Döşeyici <57726183+sinandoseyici@users.noreply.github.com>
2022-07-05 17:30:04 +0300

    Add files via upload
    Uploaded project that spring boot initilazr created with dependencies..

---
+++ src/main/java/com/codebooth/springusercommentstaff/SpringUserCommentStaffApplication.java
@@ -0,0 +1,13 @@
+package com.codebooth.springusercommentstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserCommentStaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserCommentStaffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebooth/springusercommentstaff/SpringUserCommentStaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebooth.springusercommentstaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserCommentStaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
