b245a52fa107c9de739e147e53de041783134ea8
sinandoseyici <sinandoseyici871@gmail.com>
2022-07-05 17:47:20 +0300

    Some changes

---
+++ src/main/java/com/codebooth/springusercommentstaff/service/UserService.java
@@ -0,0 +1,4 @@
+package com.codebooth.springusercommentstaff.service;
+
+public class UserService {
+}
---
+++ src/main/java/com/codebooth/springusercommentstaff/service/entitiyservice/UserEntityService.java
@@ -0,0 +1,4 @@
+package com.codebooth.springusercommentstaff.service.entitiyservice;
+
+public class UserEntityService {
+}
