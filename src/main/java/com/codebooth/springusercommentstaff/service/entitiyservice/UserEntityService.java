41afd32f3a5d480eec530a5fbafb9c586741d9e8
sinandoseyici <sinandoseyici871@gmail.com>
2022-07-05 17:53:20 +0300

    Constructed User class

--- src/main/java/com/codebooth/springusercommentstaff/service/entitiyservice/UserEntityService.java
+++ src/main/java/com/codebooth/springusercommentstaff/service/entitiyservice/UserEntityService.java
@@ -1,4 +1,5 @@
 package com.codebooth.springusercommentstaff.service.entitiyservice;
 
 public class UserEntityService {
+
 }
---
+++ src/main/java/com/codebooth/springusercommentstaff/user/User.java
@@ -0,0 +1,44 @@
+package com.codebooth.springusercommentstaff.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@Setter
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "User")
+    @SequenceGenerator(name = "User" , sequenceName = "USER_ID_SEQ", allocationSize = 1)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME", length = 50)
+    private String name;
+
+    @Column(name = "SURNAME", length = 50)
+    private String surname;
+
+    @Email
+    @Column(name = "EMAIL", length = 50)
+    private String email;
+
+    @Column(name = "PHONE_NUMBER", length = 15)
+    private String phoneNumber;
+
+    @Column(name = "BIRTH_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date birthdate;
+
+    @Transient
+    // Db'de görünsün istemiyorum.
+    private Boolean isActive;
+
+}
